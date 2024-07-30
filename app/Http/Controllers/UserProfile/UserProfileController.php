3af19548e968b54155e404386dd2d5d30a7eae4d
Agam <agamthakur99@gmail.com>
2024-07-30 17:55:26 +0530

    small changes

--- app/Http/Controllers/UserProfile/UserProfileController.php
+++ app/Http/Controllers/UserProfile/UserProfileController.php
@@ -13,14 +13,11 @@ class UserProfileController extends Controller
 
     public function userProfile()
     {
-        // dd('test user profile url');
         $users = User::where('role','user')->get();
-        // $users = User::all();
         $counts = Cart::where('user_id', auth()->user()->id)->count();
         return view('user.user_profile',compact('counts','users'));
     }
     public function editProfile(Request $request,$id){
-        // dd($request->id);
         $id = $request->id;
         $userFind = User::find($id);
         dd($userFind);
